1a3a08a06fa8bfb94c053ffb56c38513787e4879
firhan faisal <firhan@jmdigital.id>
2023-08-01 06:22:47 +0700

    fix(laravel): change Api to API

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\AuthController;
-use App\Http\Controllers\Api\PostController;
+use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\PostController;
 
 /*
 |--------------------------------------------------------------------------
